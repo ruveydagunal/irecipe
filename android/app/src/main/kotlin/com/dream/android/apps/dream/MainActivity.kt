f8d6f72f93699e1da6da52284214979261c4263d
ruveydagunal <136679910+ruveydagunal@users.noreply.github.com>
2023-10-26 17:01:49 +0300

    fix: android

--- android/app/src/main/kotlin/com/dream/android/apps/dream/MainActivity.kt
+++ android/app/src/main/kotlin/com/dream/android/apps/dream/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.irecipe
+package com.dream.android.apps.dream
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/app/views/view_onboarding/onboarding_first_view.dart
+++ lib/app/views/view_onboarding/onboarding_first_view.dart
@@ -1,7 +1,5 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
+
 
 
 class OnboardingFirstView extends StatelessWidget {
