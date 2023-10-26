fb88276d4227245e81ca77dc326ed05fd85d4b8f
ruveydagunal <136679910+ruveydagunal@users.noreply.github.com>
2023-10-26 22:14:19 +0300

    update: bloc

--- lib/app/views/view_onboarding/onboarding_view.dart
+++ lib/app/views/view_onboarding/onboarding_view.dart
@@ -2,10 +2,15 @@ import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:irecipe/app/router/app_router.dart';
 import 'package:irecipe/app/views/view_onboarding/onboarding_first_view.dart';
 import 'package:irecipe/app/views/view_onboarding/onboarding_second_view.dart';
+import 'package:irecipe/app/views/view_onboarding/onboarding_thrid_view.dart';
+import 'package:irecipe/app/views/view_onboarding/view_model/onboarding_event.dart';
 import 'package:irecipe/app/views/view_onboarding/view_model/onboarding_state.dart';
 import 'package:irecipe/app/views/view_onboarding/view_model/onboarding_view_model.dart';
+import 'package:irecipe/core/extensions/context_extension.dart';
+import 'package:irecipe/core/widgets/custom_button.dart';
 
 
 @RoutePage()
@@ -25,7 +30,18 @@ class OnboardingView extends StatelessWidget {
                     height: MediaQuery.of(context).size.height * 0.8,
                     child: bodyWidget(state),
                   ),
-             
+             Padding(
+                padding:  context.paddingNormal,
+                child: SizedBox(width: double.infinity, child: CustomButton(buttonText: 'Sonraki', onPressed: (){
+                  if(state.currentIndex<2){
+                    context
+                          .read<OnboardingViewModel>()
+                          .add(CurrentPageIndexEvent(state.currentIndex + 1));
+                  }else{
+                    context.router.replaceAll([SignUpViewRoute()]);
+                  }
+                })),
+              )
         
             ],
           ),
@@ -41,8 +57,9 @@ class OnboardingView extends StatelessWidget {
       return OnboardingFirstView();
     } else if (state.currentIndex == 1) {
       return  OnboardingSecondView();
-    }  else {
+    } else if (state.currentIndex == 2) {
+      return  OnboardingThridView(); 
+    }else {
       return Container();
-    }
-  }
+    }}
 }
