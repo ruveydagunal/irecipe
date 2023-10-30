73e8e6c212b678441313ba92e0525262fbbe2a93
ruveydagunal <136679910+ruveydagunal@users.noreply.github.com>
2023-10-30 15:06:58 +0300

    add: adding recipe view

---
+++ lib/app/views/view_adding_recipe/adding_recipe_view.dart
@@ -0,0 +1,20 @@
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class AddingRecipeView extends StatelessWidget {
+  AddingRecipeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Adding Recipe'),
+      ),
+      body: Column(
+        
+      ),
+    );
+  }
+}
